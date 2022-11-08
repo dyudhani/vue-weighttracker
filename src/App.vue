d7cc2b3fa27f66dddf6a802148c155c801bb3181
Dicky Umardhani <ddickyumardhani@gmail.com>
2022-11-08 18:20:59 +0800

    update full project

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,63 @@ import Chart from 'chart.js/auto'
 const weights = ref([])
 const weightChartEl = ref(null)
 const weightChart = shallowRef(null)
-const weightInput = ref(100.2)
+const weightInput = ref(60.0)
 
 const currentWeight = computed(() => {
-  return weightChart.value.sort((a, b) => b.date - a.date)[0] || { weight: 0 }
+  return weights.value.sort((a, b) => b.date - a.date)[0] || { weight: 0 }
 })
 
 const addWeight = () => {
-  weight.value.push({
+  weights.value.push({
     weight: weightInput.value,
     date: new Date().getTime()
   })
 }
+
+watch(weights, newWeight => {
+  const ws = [...newWeight]
+
+  if(weightChart.value){
+    weightChart.value.data.labels = ws
+      .sort((a,b) => a.date - b.date)
+      .map(w => new Date(w.date).toLocaleDateString())
+      .slice(-7)
+
+    weightChart.value.data.datasets[0].data = ws
+      .sort((a,b) => a.date - b.date)
+      .map(w => w.weight)
+      .slice(-7)
+
+    weightChart.value.update()
+
+    return
+  }
+
+  nextTick(() => {
+    weightChart.value = new Chart(weightChartEl.value.getContext('2d'), {
+      type: 'line',
+      data: {
+          labels: ws
+            .sort((a,b) => a.date - b.date)
+            .map(w => new Date(w.date).toLocaleDateString()),
+          datasets: [{
+            label: 'Weight',
+            data: ws
+              .sort((a, b) => a.date - b.date)
+              .map(w => w.weight),
+            backgroundColor: 'rgba(0, 101, 159, 0.2)',
+            borderColor: 'hwb(204 37% 2%)',
+            borderWidth: 1,
+            fill: true
+          }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    })
+  })
+}, { deep: true })
 </script>
 
 <template>
@@ -30,10 +75,37 @@ const addWeight = () => {
       <small>Current Weight (kg)</small>
     </div>
 
+    <form @submit.prevent="addWeight">
+    
+      <input type="number" step="0.1" v-model="weightInput">
+      <input type="submit" value="Add Weight">
+    
+    </form>
+
+    <div v-if="weights && weights.length > 0">
+
+      <h2>Last 7 Days</h2>
+
+      <div class="canvas-box">
+        <canvas ref="weightChartEl"></canvas>
+      </div>
+
+      <div class="weight-history">
+        <h2>Weight History</h2>
+        <ul>
+          <li v-for="weight in weights">
+            <span>
+              {{ weight.weight }}kg
+            </span>
+            <small>
+              {{ new Date(weight.date).toLocaleDateString() }}
+            </small>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+
   </main>
 
 </template>
-
-<style>
-
-</style>
